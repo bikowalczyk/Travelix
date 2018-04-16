2174dfe2406252ded05e83c2e5a7110d8bedf355
bikowalczyk <bikowalczyk@gmail.com>
2018-04-16 13:55:00 +0200

    payment modal ready

--- public/js/script.js
+++ public/js/script.js
@@ -26,10 +26,53 @@ cards.forEach((element, index) => {
         });
 
         divs[index].classList.remove("d-none"); //shows div
-        
-        
-        
+
+
+
     });
 
 })
 
+//EXITING MODAL
+
+const modal = document.querySelector("#popup");
+
+modal.addEventListener("click", () => {
+    if (location.hash == "#popup") {
+        location.hash = "#close";
+    }
+}, false);
+
+
+//FORM SUBMITING
+
+const btn = document.querySelector(".btn--submit");
+const form = document.querySelectorAll(".card-trip__form");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (form[0].checkValidity()) {
+
+        location.hash = "#popup";
+    }
+
+})
+
+
+//CONTACT US
+const popupForm = document.querySelector("#popup--form");
+const btnContact = document.querySelector(".btn--contact");
+const formContact = document.querySelector(".contact__content-form")
+
+btnContact.addEventListener("click", (e) => {
+
+    if (formContact.checkValidity()) {
+        e.preventDefault();
+        location.hash = "#popup--form";
+
+        setTimeout(() => {
+            location.hash = "#close";
+        }, 2000);
+    }
+})
