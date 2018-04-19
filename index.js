3af671bc0c0a6bdd9341ac3f5a7638577945ce3c
bikowalczyk <bikowalczyk@gmail.com>
2018-04-19 20:53:45 +0200

    server

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+var fs = require('fs');
+var http = require('http');
+
+app.set('port', (process.env.PORT || 5500))
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+})
+
+app.listen(5500, '127.0.0.1', function () {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
